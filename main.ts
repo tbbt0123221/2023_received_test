a4100b1a6bad81579b543dc25b84b986e7c06d12
tbbt0123221 <112914239+tbbt0123221@users.noreply.github.com>
2024-01-07 11:01:19 +0900

    重複防止

--- main.ts
+++ main.ts
@@ -1,254 +1,258 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.clearScreen()
-    if (速度 == 25) {
-        if (receivedNumber == 1) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 50)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 50)
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 2) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 50)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 10)
-            basic.showLeds(`
-                . # # # #
-                . . . # #
-                . . # . #
-                . # . . #
-                . # . . .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 3) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 50)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 50)
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 4) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 50)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 10)
-            basic.showLeds(`
-                # # # # .
-                # # . . .
-                # . # . .
-                # . . # .
-                . . . # .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 5) {
-            basic.showLeds(`
-                # # . . .
-                # # . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            pins.servoWritePin(AnalogPin.P15, 180)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P15, 90)
-        } else if (receivedNumber == 6) {
-            basic.showLeds(`
-                . . . # #
-                . . . # #
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            pins.servoWritePin(AnalogPin.P16, 180)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P16, 90)
-        } else if (receivedNumber == 7) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . # #
-                . . . # #
-                `)
-            pins.servoWritePin(AnalogPin.P16, 0)
-            basic.pause(1000)
-            pins.servoWritePin(AnalogPin.P16, 90)
-        } else if (receivedNumber == 8) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                # # . . .
-                # # . . .
-                `)
-            pins.servoWritePin(AnalogPin.P15, 0)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P15, 90)
-        } else if (receivedNumber == 9) {
-            basic.showLeds(`
-                # # . # #
-                # # . # #
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            pins.servoWritePin(AnalogPin.P15, 180)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P15, 180)
-            pins.servoWritePin(AnalogPin.P16, 180)
-            basic.pause(1000)
-            pins.servoWritePin(AnalogPin.P16, 90)
-            pins.servoWritePin(AnalogPin.P15, 90)
-        } else if (receivedNumber == 10) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                # # . # #
-                # # . # #
-                `)
-            pins.servoWritePin(AnalogPin.P15, -10)
-            pins.servoWritePin(AnalogPin.P16, 0)
-            basic.pause(1000)
-            pins.servoWritePin(AnalogPin.P16, 90)
-            pins.servoWritePin(AnalogPin.P15, 90)
-        }
+    if (起動中 == 0) {
+        起動中 = 1
         basic.clearScreen()
-    } else if (速度 == 0) {
-        if (receivedNumber == 1) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 25)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 25)
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 2) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 25)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 0)
-            basic.showLeds(`
-                . # # # #
-                . . . # #
-                . . # . #
-                . # . . #
-                . # . . .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 3) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 25)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 25)
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 4) {
-            motobit.enable(MotorPower.On)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 25)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 0)
-            basic.showLeds(`
-                # # # # .
-                # # . . .
-                # . # . .
-                # . . # .
-                . . . # .
-                `)
-            motobit.enable(MotorPower.Off)
-        } else if (receivedNumber == 5) {
-            basic.showLeds(`
-                # # . . .
-                # # . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            pins.servoWritePin(AnalogPin.P15, 120)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P15, 90)
-        } else if (receivedNumber == 6) {
-            basic.showLeds(`
-                . . . # #
-                . . . # #
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            pins.servoWritePin(AnalogPin.P16, 120)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P16, 90)
-        } else if (receivedNumber == 7) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . # #
-                . . . # #
-                `)
-            pins.servoWritePin(AnalogPin.P16, 60)
-            basic.pause(1000)
-            pins.servoWritePin(AnalogPin.P16, 90)
-        } else if (receivedNumber == 8) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                # # . . .
-                # # . . .
-                `)
-            pins.servoWritePin(AnalogPin.P15, 60)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P15, 90)
-        } else if (receivedNumber == 9) {
-            basic.showLeds(`
-                # # . # #
-                # # . # #
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            pins.servoWritePin(AnalogPin.P15, 180)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P15, 180)
-            pins.servoWritePin(AnalogPin.P16, 180)
-            basic.pause(1000)
-            pins.servoWritePin(AnalogPin.P16, 90)
-            pins.servoWritePin(AnalogPin.P15, 90)
-        } else if (receivedNumber == 10) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                # # . # #
-                # # . # #
-                `)
-            pins.servoWritePin(AnalogPin.P15, 0)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P15, 0)
-            pins.servoWritePin(AnalogPin.P16, 0)
-            basic.pause(1000)
-            pins.servoWritePin(AnalogPin.P16, 90)
-            pins.servoWritePin(AnalogPin.P15, 90)
+        if (速度 == 25) {
+            if (receivedNumber == 1) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 50)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 50)
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 2) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 50)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 10)
+                basic.showLeds(`
+                    . # # # #
+                    . . . # #
+                    . . # . #
+                    . # . . #
+                    . # . . .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 3) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 50)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 50)
+                basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    # . # . #
+                    . # # # .
+                    . . # . .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 4) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 50)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 10)
+                basic.showLeds(`
+                    # # # # .
+                    # # . . .
+                    # . # . .
+                    # . . # .
+                    . . . # .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 5) {
+                basic.showLeds(`
+                    # # . . .
+                    # # . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P15, 180)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            } else if (receivedNumber == 6) {
+                basic.showLeds(`
+                    . . . # #
+                    . . . # #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P16, 180)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P16, 90)
+            } else if (receivedNumber == 7) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . # #
+                    . . . # #
+                    `)
+                pins.servoWritePin(AnalogPin.P16, 0)
+                basic.pause(1000)
+                pins.servoWritePin(AnalogPin.P16, 90)
+            } else if (receivedNumber == 8) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # # . . .
+                    # # . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P15, 0)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            } else if (receivedNumber == 9) {
+                basic.showLeds(`
+                    # # . # #
+                    # # . # #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P15, 180)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P15, 180)
+                pins.servoWritePin(AnalogPin.P16, 180)
+                basic.pause(1000)
+                pins.servoWritePin(AnalogPin.P16, 90)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            } else if (receivedNumber == 10) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # # . # #
+                    # # . # #
+                    `)
+                pins.servoWritePin(AnalogPin.P15, -10)
+                pins.servoWritePin(AnalogPin.P16, 0)
+                basic.pause(1000)
+                pins.servoWritePin(AnalogPin.P16, 90)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            }
+            basic.clearScreen()
+        } else if (速度 == 0) {
+            if (receivedNumber == 1) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 25)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 25)
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 2) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 25)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 0)
+                basic.showLeds(`
+                    . # # # #
+                    . . . # #
+                    . . # . #
+                    . # . . #
+                    . # . . .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 3) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 25)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 25)
+                basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    # . # . #
+                    . # # # .
+                    . . # . .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 4) {
+                motobit.enable(MotorPower.On)
+                motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 25)
+                motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 0)
+                basic.showLeds(`
+                    # # # # .
+                    # # . . .
+                    # . # . .
+                    # . . # .
+                    . . . # .
+                    `)
+                motobit.enable(MotorPower.Off)
+            } else if (receivedNumber == 5) {
+                basic.showLeds(`
+                    # # . . .
+                    # # . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P15, 120)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            } else if (receivedNumber == 6) {
+                basic.showLeds(`
+                    . . . # #
+                    . . . # #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P16, 120)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P16, 90)
+            } else if (receivedNumber == 7) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . # #
+                    . . . # #
+                    `)
+                pins.servoWritePin(AnalogPin.P16, 60)
+                basic.pause(1000)
+                pins.servoWritePin(AnalogPin.P16, 90)
+            } else if (receivedNumber == 8) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # # . . .
+                    # # . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P15, 60)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            } else if (receivedNumber == 9) {
+                basic.showLeds(`
+                    # # . # #
+                    # # . # #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                pins.servoWritePin(AnalogPin.P15, 180)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P15, 180)
+                pins.servoWritePin(AnalogPin.P16, 180)
+                basic.pause(1000)
+                pins.servoWritePin(AnalogPin.P16, 90)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            } else if (receivedNumber == 10) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # # . # #
+                    # # . # #
+                    `)
+                pins.servoWritePin(AnalogPin.P15, 0)
+                basic.pause(500)
+                pins.servoWritePin(AnalogPin.P15, 0)
+                pins.servoWritePin(AnalogPin.P16, 0)
+                basic.pause(1000)
+                pins.servoWritePin(AnalogPin.P16, 90)
+                pins.servoWritePin(AnalogPin.P15, 90)
+            }
         }
+        basic.pause(10)
+        起動中 = 0
     }
-    basic.pause(10)
 })
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P16, 100)
@@ -278,6 +282,8 @@ function right2 () {
     motobit.enable(MotorPower.Off)
 }
 let 速度 = 0
+let 起動中 = 0
+起動中 = 0
 pins.servoWritePin(AnalogPin.P16, 90)
 pins.servoWritePin(AnalogPin.P15, 90)
 速度 = 0
